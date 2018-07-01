8e0f0bc75b25a79a923d77926f4f204088d118a5
Xarlan <egor21@gmail.com>
2018-07-01 20:45:34 +0300

    add function sniff

--- knx/knxapi.py
+++ knx/knxapi.py
@@ -4,5 +4,53 @@
 
 __author__ = 'lem'
 
+import socket
+
 import knxcore
+
+def sniff(type_medium=None, broadcast=None, timeout=45):
+    """
+    Listen broadcast and show KNXnetIP pkt
+    :param type_medium:         type of medium: tp, ip, pl, rf
+    :param broadcast:           'IP:port' for broadcast KNXnetIP '224.0.23.12:3671'
+    :param timeout:             value in [sec]
+    :return:
+    """
+
+    if type_medium is None:
+        raise Warning('You should specify the type of medium')
+
+
+    if type_medium == 'tp':
+        pass
+
+    elif type_medium == 'ip':
+        node = knxcore.FrameKnxNetIp(broadcast)
+        node.udp_socket.settimeout(timeout)
+
+    elif type_medium == 'pl':
+        pass
+
+    elif type_medium == 'rf':
+        pass
+
+    else:
+        raise Warning("Unknown type of medium")
+
+    print "\nStart sniff traffic"
+    print "Timeout [sec] = ", timeout
+
+    while True:
+
+        try:
+            node.receive_frame()
+
+        except socket.timeout:
+            break
+
+        else:
+            node.show_pkt_knx_ip()
+
+    print "\nEnded by a timeout"
+    del node
 
